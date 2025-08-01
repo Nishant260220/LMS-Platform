26cbcdc9350a53a9774a8a5f5bba2b0a46094f48
Nishant Tyagi <tyaginishu015@gmail.com>
2025-08-01 13:08:44 +0530

    Rename routes.ts to route.ts

---
+++ app/api/courses/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+import db from "@/lib/db";
+
+export async function POST(req: Request) {
+  const session = await getServerSession(authOptions);
+
+  try {
+    if (!session || !session.user?.name) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const userId = session.user.name;
+    const { title } = await req.json();
+
+    const course = await db.course.create({
+      data: {
+        userId,
+        title,
+      },
+    });
+    return NextResponse.json(course, { status: 201 });
+  } catch (error) {
+    console.log("[COURSES]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
