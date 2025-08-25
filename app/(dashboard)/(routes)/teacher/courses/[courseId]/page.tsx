8443e441d50689eb00f66257f53fe74b33d1caef
Nishant Tyagi <tyaginishu015@gmail.com>
2025-08-25 21:13:21 +0530

    feat: implement chapter form UI and create chapter API route

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -17,21 +17,29 @@ import ImageForm from "./_components/image-form";
 import CategoryForm from "./_components/category-form";
 import PriceForm from "./_components/price-form";
 import AttachmentForm from "./_components/attachment-form";
+import ChaptersForm from "./_components/chapters-form";
 
 const courseIdPage = async ({ params }: { params: { courseId: string } }) => {
   
   const { courseId } = await params;
   const session = await getServerSession(authOptions);
-
+  const userId = session?.user?.id;
+  
   if (!session?.user) {
     return redirect("/");
   }
-  
+
   const course = await db.course.findUnique({
     where: {
       id: courseId,
+      userId,
     },
     include: {
+      chapters: {
+        orderBy: {
+          position: "asc"
+        }
+      },
       attachments: {
         orderBy: {
           createdAt: "desc"
@@ -56,6 +64,7 @@ const courseIdPage = async ({ params }: { params: { courseId: string } }) => {
     course.imageURL,
     course.price,
     course.categoryId,
+    course.chapters.some(chapter => chapter.isPublished),
   ];
 
   const totalFields = requiredFields.length;
@@ -99,7 +108,7 @@ const courseIdPage = async ({ params }: { params: { courseId: string } }) => {
               <IconBadge icon={ListChecks} />
               <h2 className="text-xl4">Course chapters</h2>
             </div>
-            <div>TODO: Chapters</div>
+             <ChaptersForm initialData={course} courseId={course.id} />
             <div className="flex items-center gap-x-2">
               <IconBadge icon={CircleDollarSign} />
               <h2 className="text-xl">Sell your course</h2>
