4b53e7149635b213e7e7ebf717346a4a38e1ce5c
Nishant Tyagi <tyaginishu015@gmail.com>
2025-09-19 15:08:33 +0530

    added course progress component on coursecard

--- components/course-card.tsx
+++ components/course-card.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { IconBadge } from "./icon-badge";
 import { formatPrice } from "@/lib/format";
 import Link from "next/link";
+import { CourseProgress } from "./course-progress";
 
 
 interface CourseCardProps{
@@ -54,9 +55,11 @@ export const CourseCard = ({
                         </div>
                     </div>
                     {progress != null ? (
-                        <div>
-                            TODO: Progress component
-                        </div>
+                        <CourseProgress
+                         variant={progress === 100 ? "success" : "default"}
+                         size="sm"
+                         value={progress}
+                        />
                     ) : (
                         <p className="text-md md:text-sm font-medium text-slate-700">
                             {formatPrice(price)}
