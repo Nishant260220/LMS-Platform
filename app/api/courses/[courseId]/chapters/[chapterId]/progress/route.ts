ea391f84ad728fb79c25cfd33de2d5a9f00f5575
Nishant Tyagi <tyaginishu015@gmail.com>
2025-09-19 15:06:22 +0530

    addes onend function on video ending and create route for progress

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
@@ -0,0 +1,45 @@
+
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import db from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.id;
+
+    const { chapterId } = await params;
+
+    const { isCompleted } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const userProgress = await db.userProgress.upsert({
+      where: {
+        userId_chapterId: {
+          userId,
+          chapterId: chapterId,
+        },
+      },
+      update: {
+        isCompleted,
+      },
+      create: {
+        userId,
+        chapterId: chapterId,
+        isCompleted,
+      },
+    });
+
+    return NextResponse.json(userProgress);
+  } catch (error) {
+    console.log("[CHAPTER_ID_PROGRESS]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
