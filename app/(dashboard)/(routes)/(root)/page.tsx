fbec73560a753041d66e46f3cc6606d2642e821c
Nishant Tyagi <tyaginishu015@gmail.com>
2025-09-24 17:40:34 +0530

    showing info cards and course list on dashboard

---
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -0,0 +1,52 @@
+
+import { redirect } from "next/navigation";
+import { CheckCircle, Clock, InfoIcon } from "lucide-react";
+
+import { getDashboardCourses } from "@/actions/get-dashboard-courses";
+import { CoursesList } from "@/components/courses-list";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { InfoCard } from "./_components/info-card";
+import { Appbar } from "@/components/app";
+
+
+
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+  const userId = session?.user?.id;
+
+  if (!userId) {
+    return redirect("/signup");
+  }
+
+  const {
+    completedCourses,
+    coursesInProgress
+  } = await getDashboardCourses(userId);
+
+  return (
+    <div className="p-6 space-y-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div>
+          <InfoCard
+          icon={Clock}
+          label="In Progress"
+          numberOfItems={coursesInProgress.length}
+        />
+        </div>
+        <div>
+          <InfoCard
+          icon={CheckCircle}
+          label="Completed"
+          numberOfItems={completedCourses.length}
+          variant="success"
+        />
+        </div>
+      <CoursesList
+        items={[...coursesInProgress, ...completedCourses]}
+      />
+    </div>
+    <Appbar/>
+    </div>
+  )
+}
